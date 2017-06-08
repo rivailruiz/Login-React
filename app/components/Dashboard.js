d77ee61ffc5bd0725e79041531e4e20b376db685
Alexander Ruiz <ruizalexander@id.uff.br>
2017-06-08 05:11:44 -0300

    add files

---
+++ app/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashboard () {
+  return (
+    <article>
+      <section className='text-section'>
+        <h1>Dashboard</h1>
+        <p>
+          Você está logado.
+        </p>
+      </section>
+    </article>
+  )
+}
+
+export default Dashboard
